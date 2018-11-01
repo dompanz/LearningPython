b2cf678bb678a72194d21aba98c9bf209d5f7b3d
Dominik Panzer <dominik.panzer@icloud.com>
2018-11-01 15:14:59 +0100

    fine tuning

--- Dominik/Exercise 1/chinput.py
+++ Dominik/Exercise 1/chinput.py
@@ -1,6 +1,14 @@
+import datetime
+
+# Get the current date
+now = datetime.datetime.now()
 # Get the User Input
 name = input("Give me your name baby: ")
-date = input("And now your Birthdate " + name + " (with format DD.MM.YYYY): ")
-year = int(date[-4:])
-turnhundred = year + 100;
-print("Buo you turn 100 on " +date[:-4]+ str(turnhundred))
+#date = input("And now your Birthdate " + name + " (with format DD.MM.YYYY): ")
+age = int(input("How old are you right now, lil homy?: "))
+# Calculate the deathyear
+#year = int(date[-4:])
+turnhundred = now.year - age + 100;
+# Print it
+print("Buo you turn 100 in fucking " + str(100-age) + " years hohohoo" +
+	"\nYou will be 100 in the Year " + str(turnhundred))
